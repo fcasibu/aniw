0ce05c2f62593a4501ae6bfa521e084dcd6d637e
Francis Eugene Casibu <projectnevz@gmail.com>
2023-10-06 14:20:02 +0800

    fix: image icon being too big for smaller images

--- src/components/image/ImageWithFallback.tsx
+++ src/components/image/ImageWithFallback.tsx
@@ -7,12 +7,17 @@ import { useState } from 'react';
 
 export function ImageWithFallback({ alt, ...props }: ImageProps) {
   const [error, setError] = useState(false);
+  const width = Number(props.width || 100);
 
   return !error ? (
     <Image {...props} alt={alt} onError={() => setError(true)} />
   ) : (
     <div className="absolute h-full w-full bg-slate-800 brightness-75">
-      <ImageOff aria-hidden className="inset-center opacity-25" size={100} />
+      <ImageOff
+        aria-hidden
+        className="inset-center opacity-25"
+        size={width / 2}
+      />
     </div>
   );
 }
