26ded8890c496a74f9747d208b5a9a1823202d36
Francis Eugene Casibu <projectnevz@gmail.com>
2023-10-06 14:23:07 +0800

    feat: add presence util

--- src/utils/present.ts
+++ src/utils/present.ts
@@ -1,3 +1,7 @@
 export function present<T>(value: T): value is NonNullable<T> {
   return value != null && value !== '';
 }
+
+export function presence<T>(value: T, fallback: NonNullable<T>) {
+  return present(value) ? value : fallback;
+}
