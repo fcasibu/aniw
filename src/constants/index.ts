19d59a33cb3a53ba3a3f9430146b899e6f175d4b
Francis Eugene Casibu <projectnevz@gmail.com>
2023-10-06 18:23:07 +0800

    feat: add server api url constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL as string;
+export const SERVER_API_URL = process.env.NEXT_PUBLIC_SERVER_API_URL as string;
 
 export const ONE_WEEK_IN_SECONDS = 7 * 24 * 60 * 60;
-
 export const ONE_SECOND_IN_MS = 1000;
