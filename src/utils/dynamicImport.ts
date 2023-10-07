0a1f14102c4cf31699db95509b15f56cbf500353
Francis Eugene Casibu <projectnevz@gmail.com>
2023-10-07 15:26:46 +0800

    feat: extend function with globalOptions parameter

--- src/utils/dynamicImport.ts
+++ src/utils/dynamicImport.ts
@@ -8,12 +8,19 @@ export function dynamicImport<
   T extends ComponentType<any>,
   I extends Record<K, T>,
   K extends keyof I,
->(factory: () => Promise<I>, modules: Record<K, DynamicOptions<I> | null>) {
+>(
+  factory: () => Promise<I>,
+  modules: Record<K, DynamicOptions<I> | null>,
+  globalOptions: DynamicOptions<T> = {},
+) {
   const entries = Object.entries(modules) as [K, DynamicOptions<I> | null][];
   return Object.fromEntries(
     entries.map(([name, options]) => [
       name,
-      dynamic(() => factory().then((mod) => mod[name]), { ...options }),
+      dynamic(() => factory().then((mod) => mod[name]), {
+        ...options,
+        ...globalOptions,
+      }),
     ]),
   ) as Record<K, T>;
 }
